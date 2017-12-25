2fb3449d030dd97599de2e51897e3caf38a89ce2
Alexoriginal <Superhata23@gmail.com>
2017-12-25 03:13:01 +0200

    checkout

--- src/com/example/myproject1/withoutstart.java
+++ src/com/example/myproject1/withoutstart.java
@@ -5,5 +5,6 @@ public class withoutstart {
     public static void main(String[] args) {
         System.out.println("My project 1");
         System.out.println("My project start");
+        System.out.println("My project fix");
     }
 }
