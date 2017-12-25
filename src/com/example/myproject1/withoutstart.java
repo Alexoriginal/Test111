59c865d14d4a3b0fe5514f806a9fe09b699434e0
Alexoriginal <Superhata23@gmail.com>
2017-12-25 03:11:59 +0200

    Initial commit

---
+++ src/com/example/myproject1/withoutstart.java
@@ -0,0 +1,9 @@
+package com.example.myproject1;
+
+public class withoutstart {
+
+    public static void main(String[] args) {
+        System.out.println("My project 1");
+        System.out.println("My project start");
+    }
+}
